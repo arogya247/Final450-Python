f002187f9c3a8af951dbec1e996f961e6b8f425f
arogya247 <41734737+arogya247@users.noreply.github.com>
2021-04-10 02:50:09 +0530

    Update 30.py

--- 30.py
+++ 30.py
@@ -2,9 +2,13 @@ def findMinDiff(self, A,N,M):
     
         A.sort()
         
+        if N<M:
+            return(-1)
+        
         l = 0
         r = M-1
         min1 = A[N-1] - A[0]
+        
         while(r<N):
             min1 = min(min1,A[r]-A[l])
             #print(l," ",r," ",min1)
