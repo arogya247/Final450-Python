c2051f4027f0c678d639b75965604095e4e7a795
arogya247 <41734737+arogya247@users.noreply.github.com>
2021-04-18 01:36:31 +0530

    Create 103.py

---
+++ 103.py
@@ -0,0 +1,16 @@
+def merge(self, arr1, arr2, n, m): 
+        i=0
+        j=0
+        k=n-1
+        while i<=k and j<m:
+            if arr1[i]<arr2[j]:
+                i+=1
+            else:
+                temp = arr2[j]
+                arr2[j] = arr1[k]
+                arr1[k] = temp
+                k-=1
+                j+=1
+                
+        arr1.sort()
+        arr2.sort()
