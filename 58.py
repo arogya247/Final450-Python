f37f43f1cdc25c5677818b5a9aa4a892b63f9575
arogya247 <41734737+arogya247@users.noreply.github.com>
2021-06-01 21:29:15 +0530

    Create 58.py

---
+++ 58.py
@@ -0,0 +1,28 @@
+# Function to return the count
+# of maximum substrings str
+# can be divided into
+def maxSubStr(str, n):
+     
+    # To store the count of 0s and 1s
+    count0 = 0
+    count1 = 0
+     
+    # To store the count of maximum
+    # substrings str can be divided into
+    cnt = 0
+     
+    for i in range(n):
+        if str[i] == '0':
+            count0 += 1
+        else:
+            count1 += 1
+             
+        if count0 == count1:
+            cnt += 1
+ 
+# It is not possible to
+    # split the string
+    if count0 != count1:
+        return -1
+             
+    return cnt
